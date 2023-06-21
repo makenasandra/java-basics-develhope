5d59a20ae6d3c43a89e9c2326783652d55bba765
makenasandra <makenawachira@gmail.com>
2023-06-21 10:41:07 +0300

    ex 1

---
+++ Exercises/src/main/java/co/develhope/introduction/Start.java
@@ -0,0 +1,7 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student, 18/8/2000");
+    }
+}
